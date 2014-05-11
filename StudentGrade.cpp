372da0f046df571e3a03f270f38b1f1978344001
Anoob Soman <anoob.soman@gmail.com>
2014-05-11 20:38:36 +0100

    Median function

--- StudentGrade.cpp
+++ StudentGrade.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
 
+double calcMedian(double midTerm, double final, std::vector<double> &hw);
+
 int main()
 {
     std::string name;
     double midTerm = 0.0, final = 0.0;
-    double x = 0.0, median = 0.0;
+    double x = 0.0; 
     std::vector <double> homeWork;
-    std::vector <double>::size_type homeWorkSize = 0;
+    double median = 0.0;
 
     std::cout << "Please enter your first name: ";
     std::cin >> name;
@@ -26,15 +28,24 @@ int main()
         homeWork.push_back(x);
     }
 
-    homeWorkSize = homeWork.size();
-
-    if(homeWorkSize)
-    {
-        sort (homeWork.begin(), homeWork.end());
-        median = ((homeWorkSize % 2) == 0) ? (homeWork[homeWorkSize/2] + homeWork[(homeWorkSize/2)-1])/2 : homeWork[homeWorkSize/2];
-    }
+    median = calcMedian(midTerm, final, homeWork);
 
     std::cout << "Median " << median << std::endl;
     std::cout << "Your final grade is " << (0.2 * midTerm) + (0.4 * final) + (0.4 * median) << std::endl;
     return 0;
+}
+
+double calcMedian(double midTerm, double final, std::vector<double> &hw)
+{    
+    std::vector <double>::size_type homeWorkSize = 0;
+    double median = 0.0;
+
+    homeWorkSize = hw.size();
+
+    if(homeWorkSize)
+    {
+        sort (hw.begin(), hw.end());
+        median = ((homeWorkSize % 2) == 0) ? (hw[homeWorkSize/2] + hw[(homeWorkSize/2)-1])/2 : hw[homeWorkSize/2];
+    }
+    return median;
 }
