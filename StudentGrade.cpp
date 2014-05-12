cd33f8467e846b66e6985eeb53699b028acb0d53
Anoob Soman <anoob.soman@gmail.com>
2014-05-12 20:45:55 +0100

    Function overloading

--- StudentGrade.cpp
+++ StudentGrade.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 
-double calcMedian(double midTerm, double final, std::vector<double> &hw);
+double calcMedian(std::vector<double> hw);
+double calcGrade(double midTerm, double final, double median);
+double calcGrade(double midTerm, double final, std::vector<double> &hw);
 
 int main()
 {
@@ -9,7 +11,6 @@ int main()
     double midTerm = 0.0, final = 0.0;
     double x = 0.0; 
     std::vector <double> homeWork;
-    double median = 0.0;
 
     std::cout << "Please enter your first name: ";
     std::cin >> name;
@@ -23,20 +24,25 @@ int main()
 
     std::cout << "Please enter all your home work grades, finish by entering <eof>\n";
 
-
     while (std::cin >> x) {
         homeWork.push_back(x);
     }
 
-    median = calcMedian(midTerm, final, homeWork);
-
-    std::cout << "Median " << median << std::endl;
-    std::cout << "Your final grade is " << (0.2 * midTerm) + \
-        (0.4 * final) + (0.4 * median) << std::endl;
+    std::cout << "Your final grade is " << calcGrade(midTerm, final, homeWork) << std::endl;
     return 0;
 }
 
-double calcMedian(double midTerm, double final, std::vector<double> &hw)
+double calcGrade(double midTerm, double final, double median)
+{
+    return ((0.2 * midTerm) + (0.4 * final) + (0.4 * median));
+}
+
+double calcGrade(double midTerm, double final, std::vector<double> &hw)
+{
+    return ((0.2 * midTerm) + (0.4 * final) + (0.4 * calcMedian(hw)));
+}
+
+double calcMedian(std::vector<double> hw)
 {    
     std::vector <double>::size_type homeWorkSize = 0;
     double median = 0.0;
